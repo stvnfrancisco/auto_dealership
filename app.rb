f655bf71a188e1606951e83a16b202d056cafa2b
steven francisco <stvnfrancisco@yahoo.com>
2015-06-03 14:30:52 -0700

    vehicle class complete... front end working

---
+++ app.rb
@@ -0,0 +1,32 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/vehicle')
+also_reload('lib/**/*.rb')
+require('pry')
+
+get('/') do
+  erb(:index)
+end
+
+get('/vehicles') do
+  @vehicles = Vehicle.all()
+  erb(:vehicles)
+end
+
+get('/vehicles/new') do
+  erb(:vehicles_form)
+end
+
+post('/vehicles') do
+  make = params.fetch("make")
+  model = params.fetch("model")
+  year = params.fetch("year")
+  vehicle = Vehicle.new(make, model, year)
+  vehicle.save()
+  erb(:success)
+end
+
+get('/vehicles/:id') do
+  @vehicle = Vehicle.find(params.fetch("id").to_i())
+  erb(:vehicle)
+end
